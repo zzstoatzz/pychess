f8442b9ccac2c7612f65ab6979678b8f77e55436
Nathan Nowack <thrast36@gmail.com>
2022-03-13 00:55:20 -0600

    add colors, concept of starting position

--- Board.py
+++ Board.py
@@ -1,56 +1,85 @@
 from string import ascii_lowercase
 
+from Piece import make
+from Position import starting_position
+
 alphabet = ascii_lowercase
 
-class Square:
-    def __init__(self: object, location: str):
-        self.location = location
-        
-    def _delta(self: object, destination_square: object) -> None:
-        destination = destination_square.location
-        delta_y = abs(ord(self.location[0]) - ord(destination[0]))
-        delta_x = abs(int(self.location[1]) - int(destination[1]))
-        
-        return delta_x, delta_y
-         
-    def diagonal(self: object, destination_square: object) -> bool:
-
-        delta_x, delta_y = self._delta(destination_square)
-        
-        return delta_x == delta_y
-    
-    def touches(self: object, destination_square: object) -> bool:
-        
-        delta_x, delta_y = self._delta(destination_square)
-        
-        return delta_x < 2 and delta_y < 2
-    
-    def along(self: object, destination_square: object) -> bool:
-        
-        delta = self._delta(destination_square)
-        
-        return sum([int(i == 0) for i in delta]) == 1
-    
-    def horsey(self: object, destination_square: object) -> bool:
-        
-        delta_x, delta_y = self._delta(destination_square)
-        
-        return (delta_x == 1 and delta_y == 2) or (delta_x == 2 and delta_y == 1)
-    
-    def __repr__(self: object) -> str:
-        return self.location
 
 class Board:
     def __init__(self: object, length: int = 8):
-        self.size = length**2
+        self.size = length ** 2
         self.squares = {
-            Square(f"{letter}{number}"): None for letter in alphabet[:length] for number in range(1, length + 1)
+            f"{letter}{number}": self.Square(f"{letter}{number}")
+            for letter in alphabet[:length]
+            for number in range(1, length + 1)
         }
-        
-board = Board()
+        self.setup()
 
-test = Square('a1')
+    def setup(self: object, position: dict = starting_position):
+        for square, piece_info in position.items():
+            self.squares[square].piece = make(square, piece_info)
 
-for square in board.squares:
-    print(f"it is {square.horsey(test)} that a horse can go from {square} to {test}")
-    input()
+    def __repr__(self) -> str:
+        str_board = "\t\t\t --- PYCHESS ---"
+        for square in self.squares:
+            str_board += "  "
+            if "1" in square:
+                str_board += "\n" * 3
+            str_board += repr(self.squares[square])
+
+        return str_board
+
+    class Square:
+        def __init__(self: object, location: str):
+            self.location = location
+            self.piece = None
+
+        def _delta(self: object, destination: object) -> None:
+            destination = destination.location
+            delta_y = abs(ord(self.location[0]) - ord(destination[0]))
+            delta_x = abs(int(self.location[1]) - int(destination[1]))
+
+            return delta_x, delta_y
+
+        def along(self: object, destination: object) -> bool:
+
+            delta = self._delta(destination)
+
+            return sum([int(i == 0) for i in delta]) == 1
+
+        def diagonal(self: object, destination: object) -> bool:
+
+            delta_x, delta_y = self._delta(destination)
+
+            return delta_x == delta_y
+
+        def horsey(self: object, destination: object) -> bool:
+
+            delta_x, delta_y = self._delta(destination)
+
+            xyy = delta_x == 1 and delta_y == 2
+            xxy = delta_x == 2 and delta_y == 1
+
+            return xyy or xxy
+
+        def touches(self: object, destination: object) -> bool:
+
+            delta_x, delta_y = self._delta(destination)
+
+            return delta_x < 2 and delta_y < 2
+
+        def same_as(self: object, destination: object) -> bool:
+            return self.location == destination.location
+
+        def __repr__(self: object) -> str:
+            if self.piece:
+                return repr(self.piece)
+            else:
+                return " empty"
+
+
+if __name__ == "__main__":
+    board = Board()
+
+    print(board)
--- Piece.py
+++ Piece.py
@@ -1,42 +1,85 @@
-from Board import Square
-from typing import Callable, Iterable
+from typing import List, Union
 
 import numpy as np
+from termcolor import colored
 
 
 class Piece:
+    color: str
     material_worth: int
     kind: str
-    location: str
-    can_move: Callable
-    
+    on: Union[str, List[str]]
+
+    def remember(self: object, new_position: str):
+        try:
+            self.on.append(new_position)
+        except TypeError:
+            self.on = [self.on].append(new_position)
+
+    def __repr__(self: object):
+        color = "white" if self.color == "white" else "magenta"
+        message = colored(f"{self.kind}", color)
+
+        buffer = 16 - len(message)
+
+        return " " * (buffer // 2) + message + " " * (buffer // 2)
+
+
 class Pawn(Piece):
-    def __init__(self, location: Square):
+    def __init__(self, color: str, square_location: str):
+        self.color = color
+        self.kind = "pawn"
+        self.on = square_location
         self.material_worth = 1
-        self.kind = 'Pawn'
-        self.can_move = lambda here, there: here 
-        
+
+
 class Knight(Piece):
-    def __init__(self, location: Square):
+    def __init__(self, color: str, square_location: str):
+        self.color = color
+        self.kind = "knight"
+        self.on = square_location
         self.material_worth = 3
-        self.kind = 'Knight'
-        
+
+
 class Bishop(Piece):
-    def __init__(self, location: Square):
+    def __init__(self, color: str, square_location: str):
+        self.color = color
+        self.kind = "bishop"
+        self.on = square_location
         self.material_worth = 3
-        self.kind = 'Bishop'
+
 
 class Rook(Piece):
-    def __init__(self, location: Square):
+    def __init__(self, color: str, square_location: str):
+        self.color = color
+        self.kind = "rook"
+        self.on = square_location
         self.material_worth = 5
-        self.kind = 'Rook'
-        
+
+
 class Queen(Piece):
-    def __init__(self, location: Square):
+    def __init__(self, color: str, square_location: str):
+        self.color = color
+        self.kind = "queen"
+        self.on = square_location
         self.material_worth = 9
-        self.kind = 'Queen'
-        
+
+
 class King(Piece):
-    def __init__(self, location: Square):
+    def __init__(self, color: str, square_location: str):
+        self.color = color
+        self.kind = "king"
+        self.on = square_location
         self.material_worth = np.inf
-        self.kind = 'King'
+
+
+mappy = {"B": Bishop, "K": King, "N": Knight, "P": Pawn, "Q": Queen, "R": Rook}
+
+
+def make(square_location: str, info: tuple) -> Piece:
+
+    color, piece_type_abbrev = info
+
+    piece_type = mappy[piece_type_abbrev]
+
+    return piece_type(color, square_location)
